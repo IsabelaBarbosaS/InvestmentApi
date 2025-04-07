06486b4620e83bcf03c86be225fe1de1ee6647a8
Isabela Barbosa <55892005+IsabelaBarbosaS@users.noreply.github.com>
2025-04-07 01:05:26 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,15 +2,22 @@ using InvestmentApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSingleton<InvestmentService>();
+// Adiciona serviços ao contêiner
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
+// Configura o pipeline de requisição HTTP
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
