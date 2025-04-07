11a77062a92e55b4b5bfc95ec1851869c169f10b
Isabela Barbosa <55892005+IsabelaBarbosaS@users.noreply.github.com>
2025-04-07 01:13:59 -0300

    Delete DTOs/SimulationRequest.cs

--- DTOs/SimulationRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace InvestmentApi.DTOs
-{
-    public class SimulationRequest
-    {
-        public decimal InitialAmount { get; set; }
-        public int Months { get; set; }
-        public decimal AnnualInterestRate { get; set; }
-    }
-}
