16f99017cede27058e1aaea4f32b3961827d7250
Isabela Barbosa <55892005+IsabelaBarbosaS@users.noreply.github.com>
2025-04-08 00:16:11 -0300

    Add files via upload

--- Tests/InvestmentServiceTests.cs
+++ Tests/InvestmentServiceTests.cs
@@ -1,1 +1,28 @@
+using InvestmentApi.DTOs;
+using InvestmentApi.Services;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
 
+namespace Tests
+{
+    public class InvestmentServiceTests
+    {
+        [Fact]
+        public void Simulate_ValidInput_ReturnsPositiveRentabilidade()
+        {
+            var logger = new Mock<ILogger<InvestmentService>>();
+            var service = new InvestmentService(logger.Object);
+
+            var input = new InvestmentSimulationDto
+            {
+                ValorInicial = 1000,
+                Vencimento = DateTime.Today.AddDays(30)
+            };
+
+            var result = service.Simulate(input);
+
+            Assert.True(result.ValorFinal > input.ValorInicial);
+        }
+    }
+}
