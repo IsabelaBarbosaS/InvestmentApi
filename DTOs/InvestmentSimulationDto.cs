565be85e6ab2bbbb1055d0eafb6e5060170720cf
Isabela Barbosa <55892005+IsabelaBarbosaS@users.noreply.github.com>
2025-04-07 23:56:27 -0300

    Update InvestmentSimulationDto.cs

--- DTOs/InvestmentSimulationDto.cs
+++ DTOs/InvestmentSimulationDto.cs
@@ -1,13 +1,15 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace InvestmentApi.DTOs
 {
     public class InvestmentSimulationDto
     {
-        [Required]
+        [Required(ErrorMessage = "O valor inicial é obrigatório.")]
+        [Range(1, double.MaxValue, ErrorMessage = "O valor inicial deve ser maior que zero.")]
         public decimal ValorInicial { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "A data de vencimento é obrigatória.")]
         public DateTime Vencimento { get; set; }
     }
 }
